e2f375b474c07bd21fb0a39505b3417e0d5ec7fb
HanadiSabarnah <hsabarnah@gmail.com>
2020-11-05 15:23:05 +0200

    WTH is this.

---
+++ client/src/app.jsx
@@ -0,0 +1,23 @@
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//         }
+//     }
+   
+//     render() {
+//         return (
+//             <div>
+//                hello world
+//             </div>
+//         )
+//     }
+// }
+
+
+// ReactDOM.render(<App />, document.getElementById("app"))
+
--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 
+const express = require('express');
 
+let app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/./client/dist'))
+app.use(express.json())
+
+
+let port = 3000;
+app.listen(port, function () {
+    console.log(`listening on port ${port}`);
+  });
